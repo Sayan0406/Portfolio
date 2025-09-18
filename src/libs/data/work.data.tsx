36ffccaa2d94bcb1b04fee3153b1ac319e26ee09
Sayan0406 <114006907+Sayan0406@users.noreply.github.com>
2025-09-18 23:53:47 +0530

    Update work.data.tsx

--- src/libs/data/work.data.tsx
+++ src/libs/data/work.data.tsx
@@ -5,23 +5,12 @@ export type Work = {
   toDate: string
 }
 
-export const worksData: Work[] = [
+export const worksData = [
   {
-    company: 'Seeking a Role',
+    company: 'Job-Seeking Fresher',
     deliverable: [
       'Actively seeking a challenging Software Engineer or Frontend Developer role where I can immediately apply my expertise in React, JavaScript, and web development.',
       'Eager to contribute to innovative projects and collaborate with dynamic teams to drive impactful solutions.',
     ],
-    // fromDate: '#',
-    // toDate: '#',
   },
-  // {
-  //   company: 'NextGen Software',
-  //   deliverable: [
-  //     'Developed a full-stack e-commerce platform with product listings, cart functionality, and payment integration.',
-  //     'Integrated a payment gateway for seamless transactions.',
-  //   ],
-  //   fromDate: '2017-08',
-  //   toDate: '2019-02',
-  // },
-]
+];
